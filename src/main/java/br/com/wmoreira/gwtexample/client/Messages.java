4a20d0850fef87f4545647075f9f990d5d495db8
Wellington Moreira <wellington.362@gmail.com>
2014-12-02 10:07:38 -0200

    First commit with stub project

---
+++ src/main/java/br/com/wmoreira/gwtexample/client/Messages.java
@@ -0,0 +1,10 @@
+package br.com.wmoreira.gwtexample.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Enter your name")
+  String nameField();
+
+  @DefaultMessage("Send")
+  String sendButton();
+}
